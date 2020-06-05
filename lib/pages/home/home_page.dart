ce640b2e62ae8b8de050aee4063931e4cfee0b30
Tarciso Torres <tarcisoftorres@gmail.com>
2020-06-04 21:03:40 -0300

    Create card_app and page_view_app

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:nubank_clone/pages/home/widgets/my_app_bar.dart';
 
-class HomePage extends StatelessWidget {
+import 'widgets/page_view_app.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  
+  bool _showMenu;
+
+  @override
+  void initState() {
+    super.initState();
+    _showMenu = false;
+  }
   @override
   Widget build(BuildContext context) {
+    double _screenHeigth = MediaQuery.of(context).size.height;
+
     return Scaffold(
       backgroundColor: Colors.purple[800],
       body: Stack(
         children: <Widget>[
-          MyAppBar()
+          MyAppBar(
+            showMenu: _showMenu,
+            onTap: () {
+              setState(() {
+                _showMenu = !_showMenu;
+              });
+            },
+          ),
+          PageViewApp(top: _screenHeigth * .24,)
         ],
       ),
     );
--- lib/pages/home/widgets/my_app_bar.dart
+++ lib/pages/home/widgets/my_app_bar.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
 class MyAppBar extends StatelessWidget {
+  final bool showMenu;
+  final VoidCallback onTap;
+
+  const MyAppBar({Key key, this.showMenu, this.onTap}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -8,20 +13,35 @@ class MyAppBar extends StatelessWidget {
         SizedBox(
           height: MediaQuery.of(context).padding.top,
         ),
-        Container(
-          color: Colors.red,
-          height: 200,
-          child: Row(
-            children: <Widget>[
-              Image.network(''),
-              SizedBox(
-                width: 10,
-              ),
-              Text(
-                'Tarciso',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-              )
-            ],
+        GestureDetector(
+          onTap: onTap,
+          child: Container(
+            color: Colors.purple[800],
+            height: MediaQuery.of(context).size.height * .20,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.network(
+                      'https://logodownload.org/wp-content/uploads/2019/08/nubank-logo-3.png',
+                      height: 35,
+                      color: Colors.white,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'Tarciso',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    )
+                  ],
+                ),
+                Icon(!showMenu ? Icons.expand_more : Icons.expand_less)
+              ],
+            ),
           ),
         )
       ],
